29a0fdbad3a389babeb2571ecd3e9f130eddf338
santiagobah <privasin@icloud.com>
2024-11-06 23:25:20 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sistema de Gestión de Navegación Web
+//
+//  Created by Santiago Bañuelos on 06/11/24.
+//
+
+// Proyecto: Sistema de Gestión de Navegación Web (Historial, Páginas Activas, y Sesiones Pendientes)
+
+#include <iostream>
+#include "Navegador.h"
+using namespace std;
+
+int main() {
+    Navegador miNavegador;
+    miNavegador.VistaPrincipal();
+    return 0;
+}
